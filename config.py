e48241104951f9f2c03ae466293d60d23ea84123
Hellousak <160314800+Hellousak@users.noreply.github.com>
2025-05-19 16:11:08 +0200

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,22 @@ prefix_mapping = {
     'tp': 'Technical parameters',
     's': 'Solution',
     'm': 'Mechanical paramaters',
+
 }
 
-excluded_tags = {'lead', 'lead_h', 'None', 'ter', 'high', 'suspect_created', 'contact_updated', 'petr'}
+# slovník subkategorií
+category_tree = {
+    'Product': {
+        'intercom': ['ip_vario', 'ip_force', 'ip_base', 'ip_verso', 'ip_one', 'ip_style', 'ip_uni', 'ip_safety', 'ip_solo'],
+        'indoor_station': ['indoor_talk', 'indoor_touch', 'indoor_view', 'clip', 'ip_handset', 'indoor_compact'],
+        'software': ['ip_eye', 'network_scanner', 'access_commander', 'my2n_management_platform', 'my2n_app',
+                         'meeting_room', 'appear', 'picard_commander', 'elevator_center', 'call_centre', 'netstar'],
+        'lift': ['lift1', 'lift2', 'easygate_ip', 'easygate_pro', 'lift8', 'liftgate', 'sentrio', 'liftip_2.0'],
+        'other_product': ['voiceblue_next', 'sip_speaker_(horn)']
+        }
+    }
+
+
+
+excluded_tags = {'lead', 'lead_h', 'None', 'ter', 'high', 'suspect_created','suspect_updated', 'contact_created', 'contact_updated', 'petr'}
+
