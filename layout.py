471b158f282ff8b253b011a4cf810f54958cae49
Hellousak <160314800+Hellousak@users.noreply.github.com>
2025-05-19 16:12:20 +0200

    Update layout.py

--- layout.py
+++ layout.py
@@ -1,3 +1,4 @@
+from config import prefix_mapping, category_tree
 import streamlit as st
 
 def show_header(df, start_date, end_date):
@@ -9,26 +10,31 @@ def show_filters(df):
     min_date = df['session_start_date'].min().date()
     max_date = df['session_start_date'].max().date()
 
+    col1, col2, col3, col4, col5 = st.columns(5)
 
-    col1, col2, col3, col4 = st.columns(4)
     with col1:
         start_date = st.date_input("Start Date", value=min_date, min_value=min_date, max_value=max_date)
+
     with col2:
         end_date = st.date_input("End Date", value=max_date, min_value=min_date, max_value=max_date)
+
     with col3:
         countries = sorted(df["session_country_name"].dropna().unique())
         countries.insert(0, "All")
         selected_country = st.selectbox("Pick the Country", countries)
+
     with col4:
-        # Toto bude aktualizováno později na základě filtrování
-        all_categories = set()
-        for tags_dict in df["categorized_tags"]:
-            all_categories.update(tags_dict.keys())
-        categories = ["All"] + sorted(all_categories)
-        selected_category = st.selectbox("Choose category", categories)
+        # Získáme seznam kategorií z prefix_mapping a klíčů category_tree
+        category_tree_keys = list(category_tree.keys())
+        prefix_values = list(prefix_mapping.values())
 
-    return start_date, end_date, selected_country, selected_category
-
-
-
-
+        all_categories = ["All"] + sorted(set(prefix_values + category_tree_keys))
+        selected_category = st.selectbox("Choose category", all_categories)
+    with col5:
+        if selected_category in category_tree:
+            # Druhá úroveň: např. 'intercom', 'software', ...
+            subcategory_keys = list(category_tree[selected_category].keys())
+            selected_subcategory = st.selectbox("Choose subcategory", ["All"] + subcategory_keys)
+        else:
+            selected_subcategory = "Other"
+        return start_date, end_date, selected_country, selected_category, selected_subcategory
