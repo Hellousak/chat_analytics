21ed1771c62526ecb94c33dc242a7eff7a08d839
helens <helastekerova7@gmail.com>
2025-08-04 09:29:48 +0200

    4.8.data

--- data_loader.py
+++ data_loader.py
@@ -3,7 +3,7 @@ from utils import extract_tags, categorize_tags
 import streamlit as st
 
 
-def upload_and_load_data(default_path="reportv-16812456-o5kvPtIdIeWynnEr.csv"):
+def upload_and_load_data(default_path="reportv-16812456-2h2B9iZxZroD6QYO.csv"):
     st.sidebar.markdown("## Upload data")
 
     use_default = st.sidebar.checkbox("Use default data", value=True)
